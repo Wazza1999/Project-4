8b4ba89350588b07be70b8aef4afcef6109b76e5
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-13 08:44:24 +0000

    added secret key

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y2d+j6t1bsg%v!_ptjxy-r*lsuynqv#mo4fbrgw0r+=m_iyiwr'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
