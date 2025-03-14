65c1c930342e965028223d8da366e0ec48dbb78f
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-14 11:35:56 +0000

    changed the models.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MenuType
+from .models import MenuItem
 
 # Register your models here.
-admin.site.register(MenuType)
+admin.site.register(MenuItem)
--- menu/models.py
+++ menu/models.py
@@ -4,9 +4,19 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class MenuType(models.Model):
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = [
+        ('starter', 'Starter'),
+        ('main', 'Main'),
+        ('dessert', 'Dessert'),
+        ('drink', 'Drink'),
+    ]
+
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
