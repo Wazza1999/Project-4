3770ddc71dfb8db22913ebe713c0490316777d69
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-14 10:30:55 +0000

    Added MenuType model for menu app

--- menu/models.py
+++ menu/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class Category(models.Model):
-    name = models.Charfield(max_length=100)
+class MenuType(models.Model):
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
