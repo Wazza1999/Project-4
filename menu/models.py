ec624ab12105eb9936d22d2bb314e65905f6d112
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-14 09:49:21 +0000

    started custom model for menu app

--- menu/models.py
+++ menu/models.py
@@ -4,14 +4,6 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class Menu(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="menu"
-    )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+class Category(models.Model):
+    name = models.Charfield(max_length=100)
+    description = models.TextField(blank=True)
