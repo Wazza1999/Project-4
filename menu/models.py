4153784c19369e7e42dc065334489396723db644
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-19 20:17:28 +0000

    started changing  menu model for more customisability

--- menu/models.py
+++ menu/models.py
@@ -2,19 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class MenuItem(models.Model):
-    CATEGORY_CHOICES = [
-        ('starter', 'Starter'),
-        ('main', 'Main'),
-        ('dessert', 'Dessert'),
-        ('drink', 'Drink'),
-    ]
+class Menu(models.Model):
 
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    is_available = models.BooleanField(default=True)
+    description = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add= True)
+    date_updated = models.DateTimeField(auto_now= True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.name} - £{self.price:.2f}"
+        return self.name
