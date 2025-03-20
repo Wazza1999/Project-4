469e156cf0a6a400baf0d10832f77210c7e4c4b2
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-20 09:39:33 +0000

    added a model for individual items in the different menus

--- menu/models.py
+++ menu/models.py
@@ -9,6 +9,16 @@ class Menu(models.Model):
     date_created = models.DateTimeField(auto_now_add= True)
     date_updated = models.DateTimeField(auto_now= True)
     is_active = models.BooleanField(default=True)
-
+    
     def __str__(self):
         return self.name
+
+class Item(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    item = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items')
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+
+    def __str__(self):
+        return f"{self.name} (£{self.price})"
