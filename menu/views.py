8ced31db0ac931c3c78fda93e7d8ed75157e64a5
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-21 09:11:02 +0000

    finally creted basic html template and successfully linked views

--- menu/views.py
+++ menu/views.py
@@ -3,5 +3,7 @@ from django.views import generic
 from .models import Menu
 
 # Create your views here.
-class MenuList(generic.ListView):
-    model = Menu
+
+def menu_view(request):
+    menus = Menu.objects.prefetch_related('items').all()
+    return render(request, 'menu/menu_list.html', {'menus': menus})
