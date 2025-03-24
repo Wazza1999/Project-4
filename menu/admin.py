590d957ec9b5b181fdbc554bbe553d03bfb9a088
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-24 16:38:09 +0000

    started working with summernote

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Menu, Item
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Menu)
+class MenuAdmin(SummernoteModelAdmin):
+    name
 
 # Register your models here.
-admin.site.register(Menu)
 admin.site.register(Item)
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include("menu.urls"), name="menu-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/',
+         include('django_summernote.urls')),
     ]
