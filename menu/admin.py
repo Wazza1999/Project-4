dbd060f06f2b8087323368f036f4351110cabc43
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-25 09:16:09 +0000

    added summernote editing to admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -4,7 +4,15 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Menu)
 class MenuAdmin(SummernoteModelAdmin):
-    name
+    list_display = ('name', 'description', 'date_created', 'date_updated')
+    list_filter = ('name', 'description', 'date_created', 'date_updated')
+    search_fields = ('name', 'description', 'date_created', 'date_updated')
+
+@admin.register(Item)
+class ItemAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'description', 'item', 'price')
+    list_filter = ('name', 'description', 'item', 'price')
+    search_fields = ('name', 'description', 'item', 'price')
+
 
 # Register your models here.
-admin.site.register(Item)
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include("menu.urls"), name="menu-urls"),
     path('admin/', admin.site.urls),
-    path('summernote/',
-         include('django_summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),
     ]
